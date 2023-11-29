a7105098e7ce21c0a395659cb2a0072fb0397c5f
Aravind <aravindramsharma@gmail.com>
2023-11-29 14:48:21 +0530

    Getting a new system

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-let x;
+let x = '';
 const getNumberValue = function () {
   console.log('run');
-  x = this.value;
-  display.innerHTML = x;
+  x += this.innerHTML;
+  display.value = x;
+  console.log(typeof x);
 };
-console.log(x);
+function operate() {
+  console.log('Operating');
+}
+function clearDisplay() {
+  console.log('clearing');
+  display.value = '';
+}
 const numberButton = Array.from(document.querySelectorAll('.numberbutton'));
 const display = document.querySelector('#enter-text');
+const equalTo = document.querySelector('.equalTo');
+const clearButton = document.querySelector('.clear');
 
 numberButton.forEach((nb) => nb.addEventListener('click', getNumberValue));
+equalTo.addEventListener('click', operate);
+clearButton.addEventListener('click', clearDisplay);
